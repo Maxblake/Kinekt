95827c10d6332b7281adf438dbfd170e8a202f3a
Donovan Matsui <don.t555@gmail.com>
2019-10-10 18:35:49 -0600

    ...

--- server/server.js
+++ server/server.js
@@ -24,15 +24,17 @@ app.use("/api/admin", require("../routes/api/admin"));
 // Serve static assets in prod
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "build")));
 
-  console.log("HEY");
+  console.log(
+    "HEY HEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEYHEY"
+  );
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));

65a15df8848f664d87451e1908c7cc4f3deff0d5
Donovan Matsui <don.t555@gmail.com>
2019-10-10 18:57:41 -0600

    ...

--- server/server.js
+++ server/server.js
@@ -26,8 +26,10 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.resolve("client", "build")));
 
+  console.log(path.resolve("client", "build"));
+
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve("client", "build", "index.html"));
   });
 }
 

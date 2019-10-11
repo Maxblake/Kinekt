a13b3430c1117f793157e1a594706ba5fed353f5
Donovan Matsui <don.t555@gmail.com>
2019-10-10 18:21:50 -0600

    ...

--- server/server.js
+++ server/server.js
@@ -26,6 +26,8 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
+  console.log("HEY");
+
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
@@ -33,4 +35,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
